26151a041c94d6baf043634c6e4c1e66652c0d2b
PointBreaker <475669397@qq.com>
2023-04-26 20:58:53 +0800

    finished find

--- user/find.c
+++ user/find.c
@@ -4,12 +4,67 @@
 #include "kernel/fs.h"
 #define EQUAL 0
 
+void
+find(char *path, char *pattern, int match) {
+  char buf[512], *p;
+  int fd;
+  struct stat st;
+  struct dirent de;
+
+  if ((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch (st.type) {
+    case T_DEVICE:
+      break;
+    case T_FILE:
+      if (match == 1) {
+        printf(path);
+        printf("\n");
+      }
+      break;
+    case T_DIR:
+      if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+        printf("find: path too long\n");
+        break;
+      }
+      strcpy(buf, path);
+      p = buf + strlen(buf);
+      *p++ = '/';
+      while(read(fd, &de, sizeof(de)) == sizeof(de)){
+        if(de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+          continue;
+        memmove(p, de.name, DIRSIZ);
+        p[DIRSIZ] = 0;
+        if(stat(buf, &st) < 0){
+          printf("find: cannot stat %s\n", buf);
+          continue;
+        }
+        if (strcmp(de.name, pattern) == 0) {
+          find(buf, pattern, 1);
+        } else {
+          find(buf, pattern, 0);
+        }
+      }
+      break;
+  }
+  close(fd);
+}
+
 int
-main(int argc, char *argv[])
-{
+main(int argc, char *argv[]) {
   if (argc != 3) {
     fprintf(2, "usage: find <path> <pattern>\n");
     exit(1);
   }
+  find(argv[1], argv[2], 0);
   exit(0);
 }
